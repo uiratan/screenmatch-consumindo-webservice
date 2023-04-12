db0b587982551d176f3fe4a9f9b7ccadde890571
uiratan <uiratan@gmail.com>
2023-04-12 14:09:49 -0300

    Adicionando exception personalizada

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,64 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoException;
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        // procurar por url encoder
+        String endereco = "http://www.omdbapi.com/?t=" + busca.trim().replace(" ", "+") + "&apikey=389a3090";
+        System.out.println(endereco);
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
+
+
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println("Titulo já convertido");
+            System.out.println(meuTitulo);
+
+        } catch (NumberFormatException exception) {
+            System.out.println("Aconteceu um erro: ");
+            System.out.println(exception.getMessage());
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Aconteceu um erro de argumento na busca, verifique o endereço: ");
+            System.out.println(exception.getMessage());
+        } catch (ErroDeConversaoDeAnoException exception) {
+            System.out.println(exception.getMessage());
+        } finally {
+            System.out.println("O programa finalizou corretamente!");
+        }
+    }
+}
